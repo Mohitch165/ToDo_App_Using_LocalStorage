c6b2c885fab920387e70cdf2487e41206f9d4590
Mohit Chouhan <info.mohit321@gmail.com>
2024-11-24 19:11:38 +0000

    add feature to add tasks in the app

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("hello");
+const todoInput = document.getElementById('todo-input');
+const submitTaskBtn = document.getElementById('add-task-btn');
+const todoList = document.getElementById('todo-list');
+
+let tasks = [];
+
+submitTaskBtn.addEventListener('click', () => {
+    const inputText = todoInput.value.trim()
+    if (inputText == "") return;
+
+    const newTask = {
+        id: Date.now(),
+        task: inputText,
+        isCompleted: false
+    }
+    tasks.push(newTask);
+    todoInput.value = "" //Clear Input
+});
