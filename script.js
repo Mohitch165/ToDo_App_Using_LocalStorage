ed913a88f99f564746460613d4093d63a894ba33
Mohit Chouhan <info.mohit321@gmail.com>
2024-11-25 22:20:14 +0000

    Add functionality to render saved tasks and append them to the unordered list, add comments throughout the code for better understanding

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const todoInput = document.getElementById('todo-input');
-const submitTaskBtn = document.getElementById('add-task-btn');
-const todoList = document.getElementById('todo-list');
+    const submitTaskBtn = document.getElementById('add-task-btn');
+    const todoList = document.getElementById('todo-list');
 
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-tasks.forEach((task) => renderTask(task));
+    tasks.forEach((task) => renderTask(task));
 
-submitTaskBtn.addEventListener('click', () => {
+    submitTaskBtn.addEventListener('click', () => {  //Event to grab task from the input, create an object with data and push it to our local variable
     const inputText = todoInput.value.trim()
     if (inputText == "") return;
 
@@ -19,13 +19,26 @@ submitTaskBtn.addEventListener('click', () => {
     tasks.push(newTask);
     saveTasks();
     todoInput.value = "" //Clear Input
-});
+    });
 
-function renderTask(task) {
-}
+    function renderTask(task) {  // function to display our tasks on the webpage
+        const li = document.createElement('li');
+        li.setAttribute("data-id", task.id);
+        if(task.isCompleted) li.classList.add('completed'); // Adds CSS property of strike-through
+        li.innerHTML = 
+        `<span>${task.task}</span>
+        <button>delete</delete>`; // Grabs task content and adds it to the li
+        li.addEventListener('click', (e) => {
+            if(e.target.tagName === 'BUTTON') return;
+            task.isCompleted = !task.isCompleted
+            li.classList.toggle('completed')
+            saveTasks()
+        })
+        todoList.appendChild(li);
+    }
 
-function saveTasks() {
+    function saveTasks() { //function to save our tasks to local storage on the browser
     localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+    }
 
 });
